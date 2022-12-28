139886173eca9a7e6bd2d62382f5b291ae6a3537
junka <wan.junjie@foxmail.com>
2022-12-28 14:57:24 +0800

    teletext: fix bits order
    Signed-off-by: junka <wan.junjie@foxmail.com>

--- src/teletext.c
+++ src/teletext.c
@@ -9,8 +9,249 @@
 #include "teletext.h"
 #include "utils.h"
 
+//8 magazine 100 pages 
+static uint8_t g_text[8][100][32][40];
+
+void composite_teletext_pages(int X, int P, int Y, int z, uint8_t v)
+{
+    g_text[X][P][Y][z] = v;
+}
+
+static void dump_teletext_page_oneline(int X, int P, int Y)
+{
+    char line[8];
+    char buf[41] = "                                        ";
+    snprintf(line, 8, "X/%d", Y);
+
+    for (int i = 0; i < 40; i ++) {
+        char ch = g_text[X][P][Y][i] & 0x7F;
+        buf[i] = ch;
+    }
+    rout(1, line, "%s", buf);
+}
+
+void dump_teletext_pages(int X, int P)
+{
+    char page[32];
+    snprintf(page, 32, "Page %d", P);
+    rout(0, page, NULL);
+    for (int i = 1; i <= 25; i++) {
+        dump_teletext_page_oneline(X, P, i);
+    }
+}
+
+static inline bool odd_parity8_test(uint8_t value)
+{
+    return (__builtin_popcount(value) % 2) == 1;
+}
+
+static inline bool odd_parity24_test(uint8_t v1, uint8_t v2, uint8_t v3)
+{
+    return ((__builtin_popcount(v1) + __builtin_popcount(v2) + __builtin_popcount(v3))% 2) == 1;
+}
+
+#define REVERSE_BIT(x, v) ((~(1<<v)) & x)
+
+static unsigned char bits4_lookup[16] = {
+    0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe,
+    0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf
+};
+
+uint8_t reverse_byte(uint8_t n) {
+   // Reverse the top and bottom nibble then swap them.
+   return (bits4_lookup[n & 0xf] << 4) | bits4_lookup[n>>4];
+}
 
 
+#define REVERSE_BYTE(n) reverse_byte(n)
+
+// #define HAMMING8_DATA(x) ((((x & 0x55) >> 3) & 0x8) | (((x & 0x55) >> 2) & 0x4) | (((x & 0x55) >> 1) & 0x2) | (((x & 0x55)) & 0x1))
+
+/* rev order */
+#define HAMMING8_DATA(x) ((((x & 0x55) >> 6) & 0x1) | (((x & 0x55) >> 3) & 0x2) | (((x & 0x55)) & 0x4) | (((x & 0x55) << 3) & 0x8))
+
+//#define HAMMING24_DATA(x) ((x[2]&0xFE) >>1 | (x[1]&0xFE) << 6 | ((x[0] & 0x2E) << 13) & 0x7 |  ((x[0] & 0x2E) << 12) & 0x8)
+
+#define HAMMING24_DATA(x) ((REVERSE_BYTE(x[2]) & 0x7F) << 11 | ((REVERSE_BYTE(x[1]) & 0x7F) << 4) | ((REVERSE_BYTE(x[0]) & 0x74) >> 3) & 0xE |  ((REVERSE_BYTE(x[0]) & 0x74) >> 2) & 0x1)
+
+
+int decode_hamming8_code(uint8_t code)
+{
+    bool A = odd_parity8_test(code & 0xC5);
+    bool B = odd_parity8_test(code & 0x71);
+    bool C = odd_parity8_test(code & 0x5C);
+    bool D = odd_parity8_test(code & 0xFF);
+    if (A && B && C) {
+        if (D) {
+            /* all correct */
+            return code;
+        } else {
+            /* error in P4 */
+            return REVERSE_BIT(code, 1);
+        }
+    } else {
+        if (D) {
+            /*double error , reject */
+            return -1;
+        } else {
+            if (A && B && !C) {
+                /* P3 */
+                return REVERSE_BIT(code, 3);
+            } else if ( A && C && !B) {
+                /* P2 */
+                return REVERSE_BIT(code, 5);
+            } else if (!A && B && C) {
+                /* P1 */
+                return REVERSE_BIT(code, 7);
+            } else if (!A && !B && C) {
+                /* D4 */
+                return REVERSE_BIT(code, 0);
+            } else if (!A && !C && B) {
+                /* D3 */
+                return REVERSE_BIT(code, 2);
+            } else if (A && !B && !C) {
+                /* D2 */
+                return REVERSE_BIT(code, 4);
+            } else {
+                /* D1 */
+                return REVERSE_BIT(code, 6);
+            }
+        }
+    }
+    return 0;
+}
+
+int decode_hamming24_code(uint8_t data[3])
+{
+    bool A = odd_parity24_test(data[0]&0xAA, data[1]&0xAA, data[2]&0xAA);
+    bool B = odd_parity24_test(data[0]&0x66, data[1]&0x66, data[2]&0x66);
+    bool C = odd_parity24_test(data[0]&0x1E, data[1]&0x1E, data[2]&0x1E);
+    bool D = odd_parity24_test(data[0]&1, data[1]&0xFE, 0);
+    bool E = odd_parity24_test(0, data[1]&0x1, data[2] & 0xFE);
+    bool F = odd_parity24_test(data[0], data[1], data[2]);
+    if (A && B && C && D && E) {
+        if (F) {
+            return 0;
+        } else {
+            /* P6 */
+            *(data+2) = REVERSE_BIT(data[2], 0);
+            return 0;
+        }
+    } else {
+        if (F) {
+            return -1;
+        } else {
+            int pos = 0;
+            if (!E) {
+                pos += 1<< 4;
+            }
+            if (!D) {
+                pos += 1 << 3;
+            }
+            if (!C) {
+                pos += 1<< 2;
+            }
+            if (!B) {
+                pos += 1 << 1;
+            }
+            if (!A) {
+                pos += 1;
+            }
+            *(data + (pos - 1) / 8) = REVERSE_BIT(data[(pos - 1) / 8], pos % 8);
+            return 0;
+        }
+    }
+}
+
+/* see ETS 300706 7.1.2 */
+int decode_hamming_address(uint16_t addr, int *X)
+{
+    /* Odd numbered data bits carry the Hamming 8/4 protection bits. */
+    uint8_t magazine = (addr >> 8) & 0xFF;
+    uint8_t packet = addr & 0xFF;
+    int ret = decode_hamming8_code(magazine);
+    if (ret < 0) {
+        return -1;
+    }
+    *X = (HAMMING8_DATA(ret)) & 0x7;
+    int Y =  (HAMMING8_DATA(ret) >> 3 & 0x1) | (HAMMING8_DATA(decode_hamming8_code(packet)) << 1);
+
+    // printf("0x%x -> X %d, 0x%x -> Y %d\n", magazine, *X, packet, Y);
+    return Y;
+}
+
+int decode_odd_parity(uint8_t data)
+{
+    if (!odd_parity8_test(data)) {
+        return -1;
+    }
+    return (REVERSE_BYTE(data) & 0x7F);
+}
+
+
+int parse_teletext_data(uint16_t addr, uint8_t data[40])
+{
+    int X;
+    int Y = decode_hamming_address(addr, &X);
+    static int P = 0;
+    int ret;
+    if (Y == 0) {
+        /* page header */
+        /* three main elements: page address, control bits and data
+         * normally intended for display as described in subclause 9.3.1. */
+        ret = decode_hamming8_code(data[0]);
+        uint8_t units = HAMMING8_DATA(ret);
+        uint8_t tens = HAMMING8_DATA(decode_hamming8_code(data[1]));
+        P = tens *10 + units;
+        // printf("X %d page %d (%d, %d)\n", X, P, tens, units);
+        uint8_t s1 = HAMMING8_DATA(decode_hamming8_code(data[2]));
+        uint8_t s2 = HAMMING8_DATA(decode_hamming8_code(data[3])) & 0x7;
+        uint8_t s3 = HAMMING8_DATA(decode_hamming8_code(data[4]));
+        uint8_t s4 = HAMMING8_DATA(decode_hamming8_code(data[5])) & 0x3;
+        uint8_t erase = (HAMMING8_DATA(decode_hamming8_code(data[3])) >> 3) & 0x1;
+        uint8_t newflash = (HAMMING8_DATA(decode_hamming8_code(data[5])) >> 2) & 0x1;
+        uint8_t subtitle = (HAMMING8_DATA(decode_hamming8_code(data[5])) >> 3) & 0x1;
+        uint8_t supress_header = HAMMING8_DATA(decode_hamming8_code(data[6])) & 0x1;
+        uint8_t update_indicator = (HAMMING8_DATA(decode_hamming8_code(data[6])) >> 1) & 0x1;
+        uint8_t interrupted_sequence = (HAMMING8_DATA(decode_hamming8_code(data[6])) >> 2) & 0x1;
+        uint8_t inhibit_display = (HAMMING8_DATA(decode_hamming8_code(data[6])) >> 3) & 0x1;
+        uint8_t magazine_serial = (HAMMING8_DATA(decode_hamming8_code(data[7]))) & 0x1;
+        uint8_t national_option = (HAMMING8_DATA(decode_hamming8_code(data[7])) >> 1) & 0x7;
+        //from data 8 to 39, carry 32 bytes character or display control codes
+        //byte 32 to 39 are usually coded to represent a real-time clock
+        if (erase) {
+            for (int j = 1; j <= 28; j++) {
+                for (int i = 0; i < 40; i ++) {
+                    composite_teletext_pages(X, P, j, i, ' ');
+                }
+            }
+        }
+
+
+    } else if (Y > 25) {
+        /* non-displayable packets */
+        /* Packets with Y = 26 to 31 may also use byte 6 to extend the packet address range.
+         * Byte 6 is then Hamming 8/4 coded and is referred to as the Designation Code. 
+         */
+        uint8_t designation_code = HAMMING8_DATA(decode_hamming8_code(data[0]));
+        // decode_hamming24_code(data+3)
+        // uint32_t triplet = HAMMING24_DATA(data);
+
+        if (Y == 26) {
+
+        } else if (Y == 28) {
+
+        } else if (Y == 30) {
+
+        }
+    } else {
+        /* normal packets intended for direct display */
+        for (int i = 0; i < 40; i ++) {
+            composite_teletext_pages(X, P, Y, i, decode_odd_parity(data[i]));
+        }
+    }
+    return 0;
+}
 
 int parse_teletext(uint16_t pid, uint8_t *pbuf, int len, void *teletext)
 {
@@ -44,10 +285,11 @@ int parse_teletext(uint16_t pid, uint8_t *pbuf, int len, void *teletext)
             text->units[i].data.data_block[j] = TS_READ8(pdata);
             pdata += 1;
         }
+        parse_teletext_data(text->units[i].data.magazine_and_packet_address, text->units[i].data.data_block);
         t_len += 46;
         i ++;
     }
-    // dump_teletext(text);
+    // dump_teletext_pages(6, 6);
     return 0;
 }
 
